b79711b05515a8eeaea0ac82e60be5aac001c063
Crispioso <crispin.merriman@gmail.com>
2015-07-15 16:06:09 +0100

    Updated accordion expanding function

--- ui/js/lib/footnotes.js
+++ ui/js/lib/footnotes.js
@@ -1,25 +1,28 @@
-$(document).on('ready', function() {
-  // Expand footnotes section if it's collapsed and you click on footnote anchor
-  $('.footnote-body').on('click', function(e) {
-        var href = $(this).attr('href');
-        var footnotesSection = $(href);
-        if (footnotesSection.hasClass('is-collapsed')) {
-          footnotesSection.removeClass('is-collapsed').addClass('is-expanded');     
-        };
-  });
-
-  // If pageload links to a footnote, expand footnotes section and scroll to it
+// Expands accordion according to # at end of uri
+function expandAccordion() {
   if (location.hash) {
     var section = $(location.hash);
     if (section.hasClass('is-collapsed')) {
       section.removeClass('is-collapsed').addClass('is-expanded');
       section.get(0).scrollIntoView();
     }
-    // var $fragment = $(location.hash);
-    // var $fragmentContainer = $fragment.parents('.accordion--footnotes');
-    // if ($fragmentContainer.length) {
-    //   $fragmentContainer.removeClass('is-collapsed').addClass('is-expanded');
-    //   $fragment.get(0).scrollIntoView();
-    // }
   }
+}
+
+$(document).on('ready', function() {
+  // Expand footnotes section if it's collapsed and you click on footnote anchor
+  $('.footnote-body').on('click', function(e) {
+    expandAccordion();
+  });
+
+  // If page-load links to footnote, expand accordion and scroll to it
+  expandAccordion();
+
+  // Original pattern library solution
+  // var $fragment = $(location.hash);
+  // var $fragmentContainer = $fragment.parents('.accordion--footnotes');
+  // if ($fragmentContainer.length) {
+  //   $fragmentContainer.removeClass('is-collapsed').addClass('is-expanded');
+  //   $fragment.get(0).scrollIntoView();
+  // }
 });
