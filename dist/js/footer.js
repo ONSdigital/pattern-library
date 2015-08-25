cd0442a701e60a03f7ae9480dc8115caac3bcada
Crispioso <crispin.merriman@gmail.com>
2015-08-25 16:01:11 +0100

    Skip to main content link more visible

--- dist/js/footer.js
+++ dist/js/footer.js
@@ -19687,7 +19687,7 @@ jQuery.onFontResize=function(e){return e(document).ready(function(){var t=e("<if
 // @compilation_level SIMPLE_OPTIMIZATIONS
 
 /**
- * @license Highcharts JS v4.1.8 (2015-08-20)
+ * @license Highcharts JS v4.1.8-modified ()
  *
  * (c) 2009-2014 Torstein Honsi
  *
@@ -19740,7 +19740,7 @@ var UNDEFINED,
 	charts = [],
 	chartCount = 0,
 	PRODUCT = 'Highcharts',
-	VERSION = '4.1.8',
+	VERSION = '4.1.8-modified',
 
 	// some constants for frequently used strings
 	DIV = 'div',
@@ -20954,8 +20954,8 @@ defaultOptions = {
 	global: {
 		useUTC: true,
 		//timezoneOffset: 0,
-		canvasToolsURL: 'http://code.highcharts.com/4.1.8/modules/canvas-tools.js',
-		VMLRadialGradientURL: 'http://code.highcharts.com/4.1.8/gfx/vml-radial-gradient.png'
+		canvasToolsURL: 'http://code.highcharts.com/4.1.8-modified/modules/canvas-tools.js',
+		VMLRadialGradientURL: 'http://code.highcharts.com/4.1.8-modified/gfx/vml-radial-gradient.png'
 	},
 	chart: {
 		//animation: true,
@@ -23485,7 +23485,24 @@ SVGRenderer.prototype = {
 				// the created element must be assigned to a variable in order to load (#292).
 				imageElement = createElement('img', {
 					onload: function () {
+
+						// Special case for SVGs on IE11, the width is not accessible until the image is 
+						// part of the DOM (#2854).
+						if (this.width === 0) { 
+							css(this, {
+								position: ABSOLUTE,
+								top: '-999em'
+							});
+							document.body.appendChild(this);
+						}
+
+						// Center the image
 						centerImage(obj, symbolSizes[imageSrc] = [this.width, this.height]);
+
+						// Clean up after #2854 workaround.
+						if (this.parentNode) {
+							this.parentNode.removeChild(this);
+						}
 					},
 					src: imageSrc
 				});
@@ -37430,7 +37447,7 @@ if (seriesTypes.column) {
 
 
 /**
- * Highcharts JS v4.1.8 (2015-08-20)
+ * Highcharts JS v4.1.8-modified ()
  * Highcharts module to hide overlapping data labels. This module is included by default in Highmaps.
  *
  * (c) 2010-2014 Torstein Honsi
